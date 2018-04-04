0963a4661f1b430c03a3f42d41ba767610d9733a
JHONATHA NUNES SILVEIRA <16254855@CFP127ALUNO.LOCAL>
2018-04-04 11:25:02 -0300

    v1.0

---
+++ app/src/main/java/br/com/portalautocenter/app/Produto.kt
@@ -0,0 +1,8 @@
+package br.com.portalautocenter.app
+
+/**
+ * Created by 16254855 on 04/04/2018.
+ */
+class Produto(var id:Int, var nome:String, var preco:Double, var descricao:String, var idSubcategoria:Int,
+              var idMarcaProduto:Int, var idFilial:Int, var imagem:String){
+}
---
+++ app/src/main/java/br/com/portalautocenter/app/ProdutoAdapter.java
@@ -0,0 +1,65 @@
+package br.com.portalautocenter.app;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.squareup.picasso.Picasso;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+/**
+ * Created by 16254855 on 04/04/2018.
+ */
+
+public class ProdutoAdapter extends ArrayAdapter<Produto> {
+    public ProdutoAdapter(Context context, ArrayList<Produto> lstProdutos){
+        super(context, 0, lstProdutos);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+
+        if (v == null){
+            v = LayoutInflater.from(getContext()).inflate(R.layout.list_produtos, null);
+        }
+        //Pega o produto de determinada posição na lista
+        Produto produto = getItem(position);
+
+        //Declaração dos objetos do Layout
+        TextView txtProduto = (TextView)v.findViewById(R.id.txt_produto);
+        TextView txtDesc = (TextView)v.findViewById(R.id.txt_desc);
+        TextView txtPreco = (TextView)v.findViewById(R.id.txt_preco);
+
+        double precoProduto = produto.getPreco();
+
+        //Converte para real
+        Locale brasil = new Locale("pt", "BR");
+        NumberFormat nf = NumberFormat.getCurrencyInstance(brasil);
+
+        String preco = nf.format(precoProduto);
+
+        ImageView imgProduto = (ImageView)v.findViewById(R.id.imagem_produto);
+
+        //Colocando a imagem como Picasso
+        String url =    "http://10.0.2.2/inf4m/CAMINHO"+produto.getImagem();
+        Picasso.with(getContext()).load(url).into(imgProduto);
+
+        txtProduto.setText(produto.getNome());
+        txtDesc.setText(produto.getDescricao());
+        txtPreco.setText(preco);
+
+        return v;
+    }
+}
